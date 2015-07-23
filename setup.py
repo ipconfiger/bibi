ebb33c963ad383c7a664a284a00bb335ebccc1b6
ipconfiger <superpowerlee@gmail.com>
2015-07-24 02:38:54 +0800

    提交一个版本

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'liming'
 from setuptools import setup
 
 setup(name='bibi',
-      version='0.1.4',
+      version='0.1.5',
       description='Simple way to publish your blog',
       url='https://github.com/ipconfiger/bibi',
       author='Alexander.Li',
